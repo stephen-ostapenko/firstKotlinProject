9b6db4b42a24f7ba52d91806e6e1e92887101843
Stephen Ostapenko <stepanos2002@gmail.com>
2020-09-14 12:08:04 +0300

    First working version

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,9 @@
 fun main(args: Array <String>) {
     val sides : Array <Int> = arrayOf(args[0].toInt(), args[1].toInt(), args[2].toInt())
     sides.sort()
-    for (s in sides) {
-        println(s)
+    if (sides[0] + sides[1] > sides[2]) {
+        println("Yes")
+    } else {
+        println("No")
     }
 }
