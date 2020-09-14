87a014fe82a8aff86107c2b8567da1817c647f63
Stephen Ostapenko <stepanos2002@gmail.com>
2020-09-14 11:58:02 +0300

    Added sorting of sides by length

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,6 @@
 fun main(args: Array <String>) {
     val sides : Array <Int> = arrayOf(args[0].toInt(), args[1].toInt(), args[2].toInt())
+    sides.sort()
     for (s in sides) {
         println(s)
     }
