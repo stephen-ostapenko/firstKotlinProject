6b4ebec419933963ab9e65c0d8bf1b49cb2f3a3a
Stephen Ostapenko <stepanos2002@gmail.com>
2020-09-14 12:09:56 +0300

    Added an assertion for user to put three integer numbers

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,8 @@
 fun main(args: Array <String>) {
+    if (args.size != 3) {
+        println("Put just three integer numbers")
+        return
+    }
     val sides : Array <Int> = arrayOf(args[0].toInt(), args[1].toInt(), args[2].toInt())
     sides.sort()
     if (sides[0] + sides[1] > sides[2]) {
